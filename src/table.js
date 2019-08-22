aecd8703da7cf034563e25b38a10cffcfdebe12b
Phaneendra Varanasi <varanasikrishnaphaneendra@gmail.com>
2019-08-22 21:17:43 +0530

    Implement validation login

--- src/table.js
+++ src/table.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 
 class DrawTable extends React.Component {
     constructor(props) {
@@ -18,7 +19,53 @@ class DrawTable extends React.Component {
             this.state.gameArray2.push(row);
         }
     }
+    validateRowData(row, cellValue) {
+        var result = false;
+        console.log('validate row data', row, $('#element' + row).children().length);
+        for (var i = 1; i <= 9; i++) {
+            console.log('scan for row', 'element', row + i);
+            var data = Number($("#element" + row + i).innerHTML);
+            console.warn('data', data);
+        }
+        return true;
+    }
+    validateColData(col, cellValue) {
+        var result = false;
+        for (var i = 1; i <= 9; i++) {
+            var _cellData = Number(document.getElementById("element" + i + col).innerHTML);
+            console.log('sacan for col', 'element' + i + col,cellValue,_cellData);
+            if (_cellData === cellValue) {
+                break;
+            }
+            else {
+                result = true;
+            }
+        }
+        return result;
+    }
+    setData(e, row, col) {
 
+    }
+    clickOnCell(e, row, col) {
+        console.log('click on cell', e.target, e.target.tagName, row, col);
+        var typeCount = Number(e.target.getAttribute('type'));
+        if (typeCount === 9) {
+            typeCount = 0;
+        }
+        if (typeCount === 0) {
+            typeCount = 1;
+        }
+        else if (typeCount > 0) {
+            typeCount = typeCount + 1;
+        }
+        e.target.setAttribute('type', typeCount);
+        console.warn('before assignement type count', typeCount);
+        if (this.validateRowData(row, typeCount) && this.validateColData(col, typeCount)) {
+            e.target.innerHTML = typeCount;
+        } else {
+
+        }
+    }
     render() {
         console.log(this.state);
         return (
@@ -29,9 +76,12 @@ class DrawTable extends React.Component {
                         {
                             this.state.gameArray1.map((row, i) => {
                                 var cols = this.state.gameArray2[i].map((col, j) => {
-                                    return (<td data-row={row} data-col={col} className={(j+1)%3!==0?"game-row-sep":"group-col"} key={j}>{col}</td>)
+                                    return (
+                                        <td id={`element${row}${col}`} data-id={`element${row}${col}`} onClick={e => this.clickOnCell(e, row, col)} data-row={row} data-col={col} className={(j + 1) % 3 !== 0 ? "game-row game-row-sep" : "game-row group-col"} key={j}>
+
+                                        </td>)
                                 });
-                                return (<tr className={(i+1)%3!==0?"game-row-sep":"group"} key={i} data-row={row}>{cols}</tr>)
+                                return (<tr id={`element${row}`} data-id={`element${row}`} className={(i + 1) % 3 !== 0 ? "game-row-sep" : "group"} key={i} data-row={row}>{cols}</tr>)
                             })
                         }
                     </tbody>
